58a26958e365297cc363763626f3aef51290f3c1
Isma1303 <150766455+Isma1303@users.noreply.github.com>
2024-09-21 18:05:07 -0600

    Add files via upload

---
+++ AppState.cs
@@ -0,0 +1,26 @@
+﻿using BibliotecaHerecia.Clases;
+
+namespace BibliotecaHerecia
+{
+    public class AppState
+    {
+        private static AppState instance;
+        public Biblioteca bibliotecaActual { get; set; }
+
+        private AppState()
+        {
+            bibliotecaActual = new Biblioteca();
+        }
+
+        public static AppState Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new AppState();
+                return instance;
+            }
+        }
+    }
+
+}
---
+++ Menu.cs
@@ -0,0 +1,32 @@
+﻿using BibliotecaHerecia.Libros;
+using BibliotecaHerecia.Miembros;
+using BibliotecaHerecia.Prestamos;
+
+namespace BibliotecaHerecia
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdminMiembros_Click(object sender, EventArgs e)
+        {
+            Form verMiembrosForm = new VerMiembros();
+            verMiembrosForm.Show();
+        }
+
+        private void btnAdminLibros_Click(object sender, EventArgs e)
+        {
+            Form verLibrosForm = new VerLibros();
+            verLibrosForm.Show();
+        }
+
+        private void btnRealizarPrestamo_Click(object sender, EventArgs e)
+        {
+            Form realizarPrestamoForm = new RealizarPrestamo();
+            realizarPrestamoForm.Show();
+        }
+    }
+}
